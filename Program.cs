0faef78bf1ad4065046e14b12d892eed2fe187ab
parkerwatts <parkerwatts@PARKERWATTS09B3>
2024-01-17 16:52:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Mission_2;
+using System.Linq;
+
+//Parker Watts Group 3-7 Mission #2
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Start the welcome message
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+
+        //Ask User how many rolls to simulate and set the variable.
+
+        System.Console.Write("How many dice rolls would you like to simulate? ");
+        int NumRolls = int.Parse(System.Console.ReadLine());
+
+        //Start Game - Simulate dice rolls and add that to the int array
+
+        int[] ResultsArray = Mission_2.SimulateRoll.RollDice(NumRolls);
+
+        //Give the results message
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + NumRolls + ".\n");
+
+        // Find the percent of each result by using the count function
+
+        for (int iCount = 2; iCount < 13; iCount++)
+        {
+            float percentage = ((float)ResultsArray.Count(value => value == iCount) / NumRolls) * 100;
+
+            string sOutput = "";
+
+            //Use the Percentage to print out the histogram
+
+            for (int iCount2 = 0; iCount2 < percentage; iCount2++)
+            {
+                sOutput = sOutput + "*";
+            }
+
+            System.Console.WriteLine(iCount + ": " + sOutput);
+
+        }
+
+        //Display the end message
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ SimulateRoll.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    internal class SimulateRoll
+    {
+        
+        //Constructor
+        public SimulateRoll() 
+         {
+             // Nothing in setup
+         }
+
+        //Public Method called RollDice that has the paramater rolls
+        public static int[] RollDice(int Rolls)
+        {
+            //Create the Int array and random object to use random method later
+
+            System.Random random = new System.Random();
+
+            int[] results = new int[Rolls];
+
+            // Simulate the TWO 6 SIDED DICE rolling and put the results in a list
+
+            for (int iCount = 0; iCount < Rolls; iCount++)
+            {
+
+                int diceOne = random.Next(1, 7);
+
+                int diceTwo = random.Next(1, 7);
+
+                int diceResult = (diceOne + diceTwo);
+
+                results[iCount] = diceResult;
+
+            }
+            // The second class returns the array with the number of total rolls to the first class
+
+            return results;
+
+        }
+    }
+}
